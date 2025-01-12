50d55f24680298da5656cda152f5e273ad61490a
faiz-gear <1824056030@qq.com>
2025-01-13 00:43:56 +0800

    refactor: update SignInPage to handle asynchronous searchParams

--- src/app/(auth-pages)/sign-in/page.tsx
+++ src/app/(auth-pages)/sign-in/page.tsx
@@ -1,7 +1,9 @@
 import LoginForm from "@/components/Auth/LoginForm";
 import { Message } from "@/components/form-message";
-export default function SignInPage(props: { searchParams: Message }) {
-  const searchParams = props.searchParams;
+export default async function SignInPage(props: {
+  searchParams: Promise<Message>;
+}) {
+  const searchParams = await props.searchParams;
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
